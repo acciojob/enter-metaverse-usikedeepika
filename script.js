b22a182c321a50d4d0896375f2818893f83b52cd
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-04-19 15:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let mybutton=document.getElementById("enterBtn");
+
+mybutton.addEventListener("click",change);
+let con=document.getElementById("status");
+
+function change(){
+	con.innerHTML="<h1>Entered Metaverse</h1>"
+}
